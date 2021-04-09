7a3c4da552a803edaddd87a8bf322714895a3443
ThomasPANIER <thomaspanier.pro@gmail.com>
2021-04-09 11:31:17 +0200

    add certicate algo

--- js/slider.js
+++ js/slider.js
@@ -4,6 +4,7 @@ const images = [
     "img/certificat_comprendre_le_web.png",
     "img/certificat_wordpress.png",
     "img/certificat_html5_css3.png",
+    "img/Certificat_LeFonctionnementDesAlgorithmes.png"
 
 ];
 //déclarer descriptions
@@ -12,6 +13,7 @@ const descriptions = [
     "Certificat de réussite: Comprendre le Web",
     "Certificat de réussite: WordPress 5",
     "Certificat de réussite: Créer votre site web avec HTML5 et CSS3",
+    "Certificat de réussite: Découvrez le fonctionnement des algorithmes"
 
 ];
 
